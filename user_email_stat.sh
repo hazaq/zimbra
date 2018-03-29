cf14e6e5d781ee0a72d76a50ddd1df9bfb505c63
hazaq <hazaq.naeem@gmail.com>
2018-03-29 10:33:07 +0400

    spinner

--- user_email_stat.sh
+++ user_email_stat.sh
@@ -2,49 +2,61 @@
 
 
 usage () {
-        echo -e "\n"
-        echo "Usage:"
-        echo -e "\t$0 user@domain -s|-r [-t]"
-        echo -e "\t-s for sender or -r for recipient"
-        echo -e "\t-t to view log trace Optional"
-        echo -e "\n"
+	echo -e "\n"
+	echo "Usage:"
+	echo -e "\t$0 user@domain -s|-r [-t]"
+	echo -e "\t-s for sender or -r for recipient"
+	echo -e "\t-t to view log trace Optional"
+	echo -e "\n"
 }
 
 arg_check () {
+	
+	if [[ "$1" != *@* ]]; then
+		echo "Incorect email id "
+		usage
+		exit 0
+	fi
 
-        if [[ "$1" != *@* ]]; then
-                echo "Incorect email id "
-                usage
-                exit
-        fi
+	sanity=`su - zimbra -c "zmprov -l gaa | grep -i -e $1 "`
+	if [ -z $sanity ]; then
+		echo "$user does not exist"
+		echo "Good Bye"
+		exit 0
+	fi
 
-        sanity=`su - zimbra -c "zmprov -l gaa | grep -i -e $1 "`
-        if [ -z $sanity ]; then
-                echo "$user does not exist"
-                echo "Good Bye"
-                exit 0
-        fi
+	if [ $2 != '-r' ] && [ $2 != '-s' ] || [ -z $2 ]; then
+		echo "Incorrect argument for send or recieve"
+		usage
+		exit 0
+	fi
 
-        if [ $2 != '-r' ] && [ $2 != '-s' ]; then
-                echo "Incorrect argument for send or receive"
-                usage
-                exit 0
-        fi
-
-        if [[ $3 != '-t' ]] && [[ $3 != '' ]]; then
-                echo "Incorrect argument for trace"
-                usage
-                exit 0 
-        fi
+	if [[ $3 != '-t' ]] && [[ $3 != '' ]]; then
+		echo "Incorrect argument for trace"
+		usage
+		exit 0 
+	fi
 } 
 
+spin()
+{
+	while [ 1 ]
+	do
+		for i in ${spinner[@]};
+		do
+			echo -ne "\r$i";
+			sleep 0.2;
+		done;
+	done
+}
 
+spinner=( '\' '-' '/' '|' );
 logdays=`ls /var/log | grep -i zimbra.log- | wc -l`
 count=0
 user=$1
 check=$2
 view=$3
-arg_check $user $check $view
+arg_check $user $check $view 
 
 echo -e "\n$logdays days of log available "
 echo "Enter the number of days "
@@ -52,25 +64,29 @@ read days
 
 
 for i in `ls /var/log | grep -i zimbra.log | tail -n $days`
-do
+do 
 
-        timestamp=`echo $i | sed 's/zimbra.log-//' | sed 's/.gz//g'`
+	timestamp=`echo $i | sed 's/zimbra.log-//' | sed 's/.gz//g'` 
+	
+	if [[ $view = '-t' ]]; then
+		echo "View emails sent by user $user on $timestamp "
+		echo "Press Enter to continue..............."
+		read
+		/opt/zimbra/libexec/zmmsgtrace -s $user /var/log/$i 2> /dev/null
+	fi
+	
+	spin &
+	pid=$!
+	disown
 
-        if [[ $view = '-t' ]]; then
-                echo "View emails sent by user $user on $timestamp "
-                echo "Press Enter to continue..............."
-                read
-                /opt/zimbra/libexec/zmmsgtrace -s $user /var/log/$i 2> /dev/null
-        fi
-
-        count=$(($(/opt/zimbra/libexec/zmmsgtrace $check $user /var/log/$i 2> /dev/null | grep -i 'Message ID' | wc -l) + $count)) 
+	count=$(($(/opt/zimbra/libexec/zmmsgtrace $check $user /var/log/$i 2> /dev/null | grep -i 'Message ID' | wc -l) + $count))   
+	kill $pid
 done
 
 if [ $check = '-s' ]; then
-        word='sent'
+	word='sent'
 elif [ $check = '-r' ]; then
-        word='received'
+	word='recieved'
 fi
-echo "Total emails $word in last $days days"
+echo -e "\nTotal emails $word in last $days days"
 echo $count
-
